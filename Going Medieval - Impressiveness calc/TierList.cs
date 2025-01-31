097d0af6cda76329d865ed7fb0b73b31e29daca8
Davide Discacciati <discacciatid@gmail.com>
2025-01-31 12:48:21 +0100

    Code cleanup

---
+++ Going Medieval - Impressiveness calc/TierList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Going_Medieval___Impressiveness_calc
+{
+	internal class TierList
+	{
+		private Dictionary<string, int> tierList = new Dictionary<string, int>
+		{
+			{"Awful", 0},
+			{"Unispiring", 10 },
+			{"Modest", 15 },
+			{ "Good", 65 },
+			{"Superior", 300 },
+			{"Luxurious", 750 },
+			{"Opulent", 2500 },
+			{"Palatial", 6000 }
+		};
+
+		public string TierResult(float finalResult)
+		{
+			foreach (var tier in tierList.OrderByDescending(x => x.Value))
+			{
+				if (finalResult >= tier.Value)
+				{
+					return tier.Key;
+				}
+			}
+			return null;
+		}
+	}
+}
